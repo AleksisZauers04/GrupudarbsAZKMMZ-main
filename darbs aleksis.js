61fa41d39cbcc8a9ef1739c8f4ffbd576711ad35
AleksisZauers04 <78351807+AleksisZauers04@users.noreply.github.com>
2021-02-16 14:05:01 +0200

    first commit

---
+++ darbs aleksis.js
@@ -0,0 +1,5 @@
+window.onload=function(){
+    var sviests=document.getElementById("vieta");
+    var zim=sviests.getContext("2d");
+        zim.fillRect(10,10,100,150);
+}
---
+++ darbs.js
@@ -0,0 +1,12 @@
+function uzd1(){
+    window.location.replace("lapa1.html");
+}
+
+function home(){
+    window.location.replace("index.html");
+}
+
+function AZ(){
+    window.location.replace("aleksis.html");
+}
+
